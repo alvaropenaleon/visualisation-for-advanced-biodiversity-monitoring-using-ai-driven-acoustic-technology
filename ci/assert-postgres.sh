54a16207fe7991a93fc5ee04fd67ac50da294f53
alvaropenaleon <s3909789@student.rmit.edu.au>
2025-11-13 19:29:49 +1100

    fix: update ci smoke test stack

--- ci/assert-postgres.sh
+++ ci/assert-postgres.sh
@@ -5,8 +5,8 @@ echo "Checking Postgres responds to a simple query"
 
 # compose.yml servuce name: 'postgres'
 docker compose -f compose.yml exec -T postgres psql -U postgres -d postgres -c "SELECT 1;" || {
-    echo "Postgres query failed ❌"
+    echo "Postgres query failed"
     exit 1
 }
 
-echo "Postgres responded correctly ✅"
+echo "Postgres responded correctly"
